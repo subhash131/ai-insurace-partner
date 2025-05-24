3d93f94d3f5a50b4c81e579feb6e66b4a0293661
subhash131 <subhashnayak131@gmail.com>
2025-05-24 10:13:59 +0530

    add chat interface

--- src/app/components/chat/index.tsx
+++ src/app/components/chat/index.tsx
@@ -3,13 +3,18 @@ import { RootState } from "@/app/lib/store";
 import React from "react";
 import { useSelector } from "react-redux";
 import Sidebar from "./sidebar";
+import Messages from "./messages";
+import ChatInput from "./chat-input";
 
 const Chat = () => {
   const { messages } = useSelector((state: RootState) => state.chatReducer);
   return (
     <div className="size-full min-h-screen flex">
       <Sidebar />
-      <div>Messages</div>
+      <div className="size-full flex flex-col">
+        <Messages />
+        <ChatInput />
+      </div>
     </div>
   );
 };
---
+++ src/app/components/chat/messages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Messages = () => {
+  return <div className="size-full">Messages</div>;
+};
+
+export default Messages;
