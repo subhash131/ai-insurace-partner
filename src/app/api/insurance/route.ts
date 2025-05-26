e8366c30dfa980139e85bd712e80fd95a9c12a2f
subhash131 <subhashnayak131@gmail.com>
2025-05-26 18:54:29 +0530

    add route getInsuranceById

--- src/app/api/insurance/route.ts
+++ src/app/api/insurance/route.ts
@@ -1,6 +1,24 @@
-import { NextResponse } from "next/server";
+import { connectToDatabase } from "@/db/connection";
+import Insurance from "@/models/Insurance";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const body = await req.json();
   return NextResponse.json({ received: body });
+}
+
+export async function GET(req: NextRequest) {
+  const id = req.nextUrl.searchParams.get("id");
+  if (!id) {
+    return NextResponse.json(
+      { error: "ID parameter is required" },
+      { status: 400 }
+    );
+  }
+  await connectToDatabase();
+  const res = await Insurance.findById(id);
+  if (!res) {
+    return NextResponse.json({ error: "Insurance not found" }, { status: 404 });
+  }
+  return NextResponse.json(res);
 }
